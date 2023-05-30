4f63b5cb1ce0e3416d7e2cef9c1d9f5b90f18742
haksemin <42060402+haksemin@users.noreply.github.com>
2023-05-30 12:02:55 +0300

    Notch sorunu çözüldü statsbar +8px

--- UYVProject/App.js
+++ UYVProject/App.js
@@ -1,29 +1,46 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, StatusBar, View, StyleSheet, Platform, Dimensions } from "react-native";
 import SafeViewAndroid from "./src/components/SafeViewAndroid";
 import DeviceInfo from "react-native-device-info";
 
-
-export default function App(){
-
-  const [isNotchVisible, setIsNotchVisible] = useState(false);
+export default function App() {
+  const [hasNotch, setHasNotch] = useState(false);
 
   useEffect(() => {
     checkDeviceNotch();
   }, []);
 
   const checkDeviceNotch = async () => {
-    const hasNotch = await DeviceInfo.hasNotch();
-    setIsNotchVisible(hasNotch);
+    const deviceHasNotch = await DeviceInfo.hasNotch();
+    setHasNotch(deviceHasNotch);
   };
-  
 
+  const statusBarHeight = Platform.OS === "ios" ? 20 : StatusBar.currentHeight;
 
-
-  return(
-    <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-    <View style={{backgroundColor:"white",height:72}}></View>
-    </SafeAreaView>
-    
-  )
+  return (
+    <>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      <SafeAreaView style={[SafeViewAndroid.AndroidSafeArea, styles.container]}>
+        <View style={[styles.topBar, { backgroundColor: "white", height: statusBarHeight +8}]} />
+        {hasNotch && <View style={[styles.notchContainer, { backgroundColor: "white", marginTop: -statusBarHeight }]} />}
+        {/* Diğer içerikler */}
+      </SafeAreaView>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+  },
+  topBar: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+  notchContainer: {
+    height: 72,
+  },
+});
