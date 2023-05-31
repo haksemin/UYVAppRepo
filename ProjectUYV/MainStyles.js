1b89cc3a1741aca1874ecc4ce182cf3a911c9f7d
haksemin <42060402+haksemin@users.noreply.github.com>
2023-05-31 21:31:26 +0300

    Da dumla da

---
+++ ProjectUYV/MainStyles.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, StatusBar, Platform, View } from 'react-native';
+
+const STATUSBAR_HEIGHT = StatusBar.currentHeight;
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+
+const Mainstyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  statusBar: {
+    height: STATUSBAR_HEIGHT,
+  },
+  appBar: {
+    backgroundColor:'white',
+    height: 72,
+    justifyContent:"center",
+
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+  },
+});
+
+export default Mainstyles;
--- ProjectUYV/MainTemp.js
+++ ProjectUYV/MainTemp.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import {AppRegistry, StyleSheet,View,StatusBar,Platform,SafeAreaView,Text, Image, Touchable, TouchableOpacity} from 'react-native';
+import Mainstyles from './MainStyles';
+
+
 
 const MyStatusBar = ({backgroundColor, ...props}) => (
-  <View style={[styles.statusBar, { backgroundColor }]}>
+  <View style={[Mainstyles.statusBar, { backgroundColor }]}>
     <SafeAreaView>
       <StatusBar translucent backgroundColor={backgroundColor} {...props} />
     </SafeAreaView>
   </View>
 );
 
-
-
-
-export default function App(){
+export default function MainTemp(){
   
     return (
       <>
-        <View style={styles.container}>
+        <View style={Mainstyles.container}>
         <MyStatusBar backgroundColor="white" barStyle="dark-content" />
-        <View style={styles.appBar}>
+        <View style={Mainstyles.appBar}>
           <Image source={require("./src/images/UYVLogo.png")} style={{position:"absolute",left:15}}></Image>
           <TouchableOpacity style={{position:"absolute",height:42,width:42 ,right:15}}>
           <Image source={require("./src/images/Destek.png")} style={{height:"100%",width:"100%"}}></Image>
           </TouchableOpacity>
         </View>
-        <View style={styles.content}>
+        <View style={Mainstyles.content}>
         <Image source={require("./src/images/Vector.png")} style={{height:"100%",width:"100%"}}></Image>
           <Text>Merhaba</Text>
         </View>
@@ -43,26 +43,5 @@ export default function App(){
 
 
 
-const STATUSBAR_HEIGHT = StatusBar.currentHeight;
-const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  statusBar: {
-    height: STATUSBAR_HEIGHT,
-  },
-  appBar: {
-    backgroundColor:'white',
-    height: 72,
-    justifyContent:"center",
-
-  },
-  content: {
-    flex: 1,
-    backgroundColor: '#F5F5F5',
-  },
-});
 
 AppRegistry.registerComponent('App', () => DarkTheme);
