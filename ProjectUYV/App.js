40f4c37d4b2f4378e011c6a618aaaf8c609ce0b1
haksemin <42060402+haksemin@users.noreply.github.com>
2023-05-31 20:09:49 +0300

    İOSU ÇÖZDÜ ANDROİDİ ÇÖZER Mİ BİLEMEDİM

--- ProjectUYV/App.js
+++ ProjectUYV/App.js
@@ -1,26 +1,58 @@
-import React, { useEffect, useState } from "react";
-import { SafeAreaView, StatusBar, View, StyleSheet, Platform } from "react-native";
-import SafeViewAndroid from "./src/components/SafeViewAndroid";
+import React, {Component,} from 'react';
+import {AppRegistry, StyleSheet,View,StatusBar,Platform,SafeAreaView} from 'react-native';
+
+const MyStatusBar = ({backgroundColor, ...props}) => (
+  <View style={[styles.statusBar, { backgroundColor }]}>
+    <SafeAreaView>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </SafeAreaView>
+  </View>
+);
 
 
-export default function App() {
-  const statusBarHeight = Platform.OS === "ios" ? 20 : StatusBar.currentHeight;
 
-  return (
-    <>
-      <StatusBar backgroundColor="white" barStyle="dark-content" />
-      <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-        <View style={{ backgroundColor: "black", height: 72 }} />
-       
-        {/* Diğer içerikler */}
-      </SafeAreaView>
-    </>
-  );
-}
+
+export default function App(){
+  
+    return (
+      <>
+        <View style={styles.container}>
+        <MyStatusBar backgroundColor="white" barStyle="dark-content" />
+        <View style={styles.appBar} />
+        <View style={styles.content} />
+      </View>
+      </>
+    );
+    }
+
+
+
+
+
+
+
+
+
+
+
+const STATUSBAR_HEIGHT = StatusBar.currentHeight;
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F5F5F5",
+  },
+  statusBar: {
+    height: STATUSBAR_HEIGHT,
+  },
+  appBar: {
+    backgroundColor:'white',
+    height: APPBAR_HEIGHT,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
   },
 });
+
+AppRegistry.registerComponent('App', () => DarkTheme);
