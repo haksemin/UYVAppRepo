1458a978887f246913881e28fee82c7ccdcfca37
Haksemin <haksemin@gmail.com>
2023-05-31 17:27:21 +0300

    turnback

--- UYVProject/App.js
+++ UYVProject/App.js
@@ -7,9 +7,9 @@ import SafeViewAndroid from "./src/components/SafeViewAndroid";
 export default function App() {
     return (
         <>
-            <SafeAreaView style={SafeViewAndroid.AndroidSafeArea} />
-            <StatusBar barStyle="dark-content" />
-            <View style={{backgroundColor:"#F5F5F5",flex:1}}></View>
+            <SafeAreaView />
+            <StatusBar backgroundColor={"red"} barStyle="dark-content" />
+            <View style={{backgroundColor:"red",flex:1}}></View>
             
         </>
     );
