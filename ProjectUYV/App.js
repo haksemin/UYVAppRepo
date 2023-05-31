bca670cef00c79e0ec4deb7b518c23b515ef8264
haksemin <42060402+haksemin@users.noreply.github.com>
2023-05-31 19:23:54 +0300

    revert attım2

--- ProjectUYV/App.js
+++ ProjectUYV/App.js
@@ -1,31 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { SafeAreaView, StatusBar, View, StyleSheet, Platform } from "react-native";
-
-const iosStatsBarColor = 'red';
+import SafeViewAndroid from "./src/components/SafeViewAndroid";
 
 
 export default function App() {
-    return (
-        <>
-            <StatusBar backgroundColor={"white"} barStyle="dark-content" />
-            <SafeAreaView style={styles.topSafeArea}>
-            <View style={{backgroundColor:"black",height:72}}></View>
-            </SafeAreaView >
-            
-            
-            
-            
-            
-        </>
-    );
+  const statusBarHeight = Platform.OS === "ios" ? 20 : StatusBar.currentHeight;
+
+  return (
+    <>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+        <View style={{ backgroundColor: "black", height: 72 }} />
+       
+        {/* Diğer içerikler */}
+      </SafeAreaView>
+    </>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    topSafeArea: {
-        flex: 0, 
-        backgroundColor: iosStatsBarColor
-    },
- 
-})
-
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+});
