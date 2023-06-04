424a40fa2806257d486596481ed4f39d0c771326
haksemin <42060402+haksemin@users.noreply.github.com>
2023-06-04 14:37:44 +0300

    Cardı bitirdim

--- ProjectUYV/src/components/CarouselCard.js
+++ ProjectUYV/src/components/CarouselCard.js
@@ -17,17 +17,14 @@ export default function CarouselCard() {
         borderRadius: 10,
         top: 15,
       }}>
-      <LinearGradient
-        colors={['rgba(22, 62, 108, 0)', '#163E6C']}
-        style={{ flex: 1 }}
-      >
+      <LinearGradient colors={['rgba(22, 62, 108, 0)', '#163E6C']} style={{borderRadius:10}} >
         <Image source={require("../images/CardSlider.png")}  />
+        </LinearGradient>
         <Text style={{ fontFamily: "OpenSans-Bold", position: "absolute", left: 15, bottom: 62, color: "white", fontSize: 18, fontWeight: 700, lineHeight: 25 }}>Türkiye Acil Yardım</Text>
         <TouchableOpacity style={{ width: 315, height: 42, bottom: 15, backgroundColor: "#E74C3C", position: "absolute", alignSelf: "center", justifyContent: "center", borderRadius: 10 }}>
           <Text style={{ fontFamily: "OpenSans-Bold", fontSize: 14, lineHeight: 19, color: "white", fontWeight: 600, alignSelf: "center" }}>Bağış Yap</Text>
 
         </TouchableOpacity>
-      </LinearGradient>
     </View>
   );
 
