fcb53136387fa29d5e55ca326706f5beb326d7d8
Haksemin <haksemin@gmail.com>
2023-07-19 08:56:45 +0300

    Dropdownbox

--- ProjectUYV/src/components/UserInput/DonateKindDDown.js
+++ ProjectUYV/src/components/UserInput/DonateKindDDown.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View,Dimensions } from "react-native";
+import { View,Dimensions, Image } from "react-native";
 import { SelectList } from 'react-native-dropdown-select-list';
 const SCREEN_WIDTH = Dimensions.get('window').width;
 export default function DonateKindDDown(){
@@ -22,7 +22,8 @@ export default function DonateKindDDown(){
           placeholder={selected}
           boxStyles={{borderColor:"#B7C3D1",borderRadius:10,backgroundColor:"white",position:"absolute",width:SCREEN_WIDTH-30,height:60,alignSelf:"center"}}
           dropdownStyles={{borderColor:"#B7C3D1",borderRadius:10,backgroundColor:"white",position:"absolute",marginTop:70,width:SCREEN_WIDTH-30,alignSelf:"center"}}
-          
+          onSelect={() => console.log(selected)} 
+          arrowicon={<Image source={require('../../images/down.png')} style={{position:"absolute",right:0,marginRight:15,alignSelf:"center"}}></Image>}
       />
       </View>
       </View>
--- ProjectUYV/src/components/UserInput/userdata_int.js
+++ ProjectUYV/src/components/UserInput/userdata_int.js
@@ -22,12 +22,13 @@ export default function Userdataint() {
       <View style={{ width: SCREEN_WIDTH - 30, height: 60, margin: 15, top: 15, borderRadius: 10, borderColor: "#B7C3D1", borderWidth: 1 }}>
         <Text style={{ fontFamily: "OpenSans-Regular", fontSize: 14, fontWeight: 700, color: "#163E6C", marginTop: 5, left: 15 }}>Bağış Tutarı</Text>
         <TextInput
-          style={{ textAlignVertical: "center", fontSize: 14, left: 15, paddingBottom: 5, height: 30, width: SCREEN_WIDTH - 100, color: "black" }}
+          style={{ textAlignVertical: "center", fontSize: 14, left: 15, paddingBottom: 5, height: 30, width: SCREEN_WIDTH - 100, color: "black",fontFamily: "OpenSans-Regular", }}
           placeholder="Tutar Giriniz"
           placeholderTextColor={"black"}
           value={amount}
           onChangeText={handleAmountChange}
           keyboardType="numeric"
+          
         />
         <Text style={{ position: "absolute", fontFamily: "OpenSans-Regular", fontSize: 20, fontWeight: 900, color: "#163E6C", padding: 15, right: 0 }}>₺</Text>
       </View>
