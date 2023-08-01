1c7c4c27840dea59d6ab48df7341ceaf656b6048
Haksemin <haksemin@gmail.com>
2023-08-01 12:15:54 +0300

    guncelleme
    EklenenKisiComponent.js
    HatimOlustur.js

--- ProjectUYV/src/components/HatimComponents/EklenenKisiComponent.js
+++ ProjectUYV/src/components/HatimComponents/EklenenKisiComponent.js
@@ -1,43 +1,143 @@
-import React from "react"
-import { View,Image,Text,Dimensions } from "react-native"
-export default function EklenenKisiComponent(){
+import React, { useRef } from "react";
+import { View, Image, Text, Dimensions } from "react-native";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  runOnJS,
+} from "react-native-reanimated";
 
-  const SCREEN_WIDTH = Dimensions.get('window').width;
+export default function EklenenKisiComponent() {
+  const SCREEN_WIDTH = Dimensions.get("window").width;
+  const deleteButtonWidth = 45;
 
+  const translateX = useSharedValue(0);
 
-    return(
-        <View>
-        <View>
-          <Image source={require("../../images/hesabim/Line.png")} style={{width:SCREEN_WIDTH-30,alignSelf:"center"}}></Image>
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.startX = translateX.value;
+    },
+    onActive: (event, ctx) => {
+      translateX.value = ctx.startX + event.translationX;
+    },
+    onEnd: (event) => {
+      if (event.translationX > deleteButtonWidth / 2) {
+        translateX.value = withSpring(deleteButtonWidth);
+      } else {
+        translateX.value = withSpring(0);
+      }
+    },
+  });
 
-          
-          
-          <View style={{justifyContent:"center",marginTop:15}}>
-            <View style={{flexDirection:"row",alignItems:"center"}}>
-                <View style={{marginHorizontal:5}}>
-                    <Image source={require("../../images/adamuser.png")} style={{width:46,height:46,}}></Image>
-                </View>
-                <View style={{flexDirection:"column",marginHorizontal:5}}>
-                <Text style={{ color: "#163E6C", fontFamily: "OpenSans-Regular", fontSize: 14, fontWeight: 600, }}>
-                Fatma GEDİK
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: translateX.value }],
+    };
+  });
+
+  const onDeleteButtonPress = () => {
+    console.log("Delete button pressed!");
+  };
+
+  return (
+    <PanGestureHandler onGestureEvent={onGestureEvent}
+    activeOffsetX={[-30, 30]} 
+    >
+      <Animated.View style={[{ width: SCREEN_WIDTH-30 }, animatedStyles]}>
+        <View style={{marginVertical:10}}>
+          <Image
+            source={require("../../images/hesabim/Line.png")}
+            style={{ width: SCREEN_WIDTH - 30, alignSelf: "center" }}
+          />
+          <View style={{ justifyContent: "center", marginTop: 15 }}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <View style={{ marginHorizontal: 5 }}>
+                <Image
+                  source={require("../../images/adamuser.png")}
+                  style={{ width: 46, height: 46 }}
+                />
+              </View>
+              <View style={{ flexDirection: "column", marginHorizontal: 5 }}>
+                <Text
+                  style={{
+                    color: "#163E6C",
+                    fontFamily: "OpenSans-Regular",
+                    fontSize: 14,
+                    fontWeight: 600,
+                  }}
+                >
+                  Fatma GEDİK
                 </Text>
-                <View style={{flexDirection:"row",alignItems:"center",marginTop:5}}>
-                <Image source={require("../../images/mobilegreen.png")} style={{width:11,height:14,}}></Image>
-                <Text style={{ color: "#163E6C", fontFamily: "OpenSans-Regular", fontSize: 12, fontWeight: 400,marginHorizontal:5 }}>
-                +90 555 368 2666
-                </Text>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginTop: 5,
+                  }}
+                >
+                  <Image
+                    source={require("../../images/mobilegreen.png")}
+                    style={{ width: 11, height: 14 }}
+                  />
+                  <Text
+                    style={{
+                      color: "#163E6C",
+                      fontFamily: "OpenSans-Regular",
+                      fontSize: 12,
+                      fontWeight: 400,
+                      marginHorizontal: 5,
+                    }}
+                  >
+                    +90 555 368 2666
+                  </Text>
                 </View>
-                </View>
+              </View>
             </View>
 
-            <View style={{right:0,position:"absolute",alignSelf:"center",justifyContent:"center",flexDirection:"column",alignContent:"center",marginHorizontal:5}}>
-              <Text style={{ color: "#77A52C", fontFamily: "OpenSans-Regular", fontSize: 18, fontWeight: 700,}}>
+            <View
+              style={{
+                right: 0,
+                position: "absolute",
+                alignSelf: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+                alignContent: "center",
+                marginHorizontal: 5,
+              }}
+            >
+              <Text
+                style={{
+                  color: "#77A52C",
+                  fontFamily: "OpenSans-Regular",
+                  fontSize: 18,
+                  fontWeight: 700,
+                }}
+              >
                 1.-4. Cüz
               </Text>
             </View>
           </View>
-          
         </View>
-      </View>
-    )
+        <View
+          style={{
+            position: "absolute",
+            left: -60,
+            top: 15,
+            bottom: 0,
+            width: deleteButtonWidth,
+            backgroundColor: "#E74C3C",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onTouchEnd={() => {
+            runOnJS(onDeleteButtonPress)();
+          }}
+        >
+          <Text style={{ color: "white",fontFamily:"OpenSans-Regular",fontSize:14 }}>Sil</Text>
+        </View>
+      </Animated.View>
+    </PanGestureHandler>
+  );
 }
--- ProjectUYV/src/components/HatimComponents/HatimOlustur.js
+++ ProjectUYV/src/components/HatimComponents/HatimOlustur.js
@@ -12,7 +12,7 @@ import EklenenKisiComponent from "./EklenenKisiComponent";
 export default function HatimOlustur() {
   const SCREEN_WIDTH = Dimensions.get('window').width;
   const [modalVisible, setModalVisible] = useState(false);
-  const [modalVisibleM,setModalVisibleM] = useState(false);
+  const [modalVisibleM, setModalVisibleM] = useState(false);
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
   const [selectedDate, setSelectedDate] = useState(null);
   const navigation = useNavigation();
@@ -37,10 +37,6 @@ export default function HatimOlustur() {
     setSelectedDate(date);
     hideDatePicker();
   };
-
-
-
-
 
   function goBacktoMain() {
     navigation.navigate("Hatim Main");
@@ -74,34 +70,33 @@ export default function HatimOlustur() {
         />
       </View>
 
-      <View style={{ top: 0, height: 210 }}>
+      <View style={{ top: 0, height: 210,flex:1 }}>
         <Image source={require("../../images/hatim/ustsari.png")} style={{ width: "100%", top: 0 }} />
         <View style={{ position: "absolute", top: 15, flexDirection: "column", width: "100%" }}>
           <View style={{ flexDirection: "row", margin: 15, alignItems: "center" }}>
             <Image source={require("../../images/hatim/book-open.png")} style={{ width: 34, height: 30 }} />
-            <Text style={{ fontFamily: "OpenSans-Regular", color: "white", fontSize: 24, fontWeight: 700, marginLeft: 15 }}>Hatim Paylaş</Text>
+            <Text style={{ fontFamily: "OpenSans-Regular", color: "white", fontSize: 24, fontWeight: "700", marginLeft: 15 }}>Hatim Paylaş</Text>
           </View>
 
-          <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1 }} bounces={false} >
+          
             <View style={{ marginTop: 60 }}>
               <InputBoxNew contenttext={"Hatim Adı"} aciklama={"Hatim Adı Giriniz"}></InputBoxNew>
             </View>
-            
-
-            <View>
+        </View>
+            <View style={{ width: SCREEN_WIDTH - 30, margin: 15,top:25 }}>
               <TouchableOpacity onPress={toggleDatePicker}>
                 <View style={{ margin: 7.5, alignSelf: "center" }}>
                   <View style={{ width: SCREEN_WIDTH - 30, height: 60, borderRadius: 10, borderColor: "#B7C3D1", borderWidth: 1, backgroundColor: "white", justifyContent: "center" }}>
-                    <Text style={{ fontFamily: "OpenSans-Regular", fontSize: 16, fontWeight: 400, color: "#163E6C", marginTop: 0, left: 15 }}>Bitiş Tarihi</Text>
+                    <Text style={{ fontFamily: "OpenSans-Regular", fontSize: 16, fontWeight: "400", color: "#163E6C", marginTop: 0, left: 15 }}>Bitiş Tarihi</Text>
                     <View style={{ marginLeft: 15 }}>
                       {selectedDate && (
-                        <Text style={{ fontFamily: "OpenSans-Regular", fontSize: 14, fontWeight: 600, color: "#163E6C" }}>
+                        <Text style={{ fontFamily: "OpenSans-Regular", fontSize: 14, fontWeight: "600", color: "#163E6C" }}>
                           {`Seçilen: ${formatDate(selectedDate)}`}
                         </Text>
                       )}
                     </View>
                     <View style={{ position: "absolute", right: 0, marginRight: 15 }}>
-                      <Image source={require("../../images/down.png")}></Image>
+                      <Image source={require("../../images/down.png")} />
                     </View>
                   </View>
                 </View>
@@ -115,6 +110,8 @@ export default function HatimOlustur() {
               />
 
             </View>
+            <ScrollView showsVerticalScrollIndicator={false} horizontal={false}>           
+
             <View style={{ width: SCREEN_WIDTH - 30, margin: 15 }}>
               <Text style={{ color: "#163E6C", fontFamily: "OpenSans-Regular", fontSize: 14, fontWeight: "700" }}>Hatim Paylaşılacak Kişiler</Text>
               <TouchableOpacity onPress={toggleModalM}>
@@ -122,28 +119,33 @@ export default function HatimOlustur() {
                   <Text style={{ color: "#163E6C", fontFamily: "OpenSans-Regular", fontSize: 16, fontWeight: "600" }}>Kişi Ekle +</Text>
                 </View>
               </TouchableOpacity>
-              <View>
-                <EklenenKisiComponent></EklenenKisiComponent>
-              </View>
+              
+                
+                  
+                  <EklenenKisiComponent></EklenenKisiComponent>
+                  
+                  
+                
+              
               <View style={{ borderStyle: "dashed", borderColor: "#77A52C", height: 60, width: SCREEN_WIDTH - 30, backgroundColor: "#F1F6EA", borderWidth: 2, borderRadius: 5, marginTop: 15, justifyContent: "center", alignItems: "center" }}>
                 <Text style={{ color: "#163E6C", fontFamily: "OpenSans-Regular", fontSize: 14, fontWeight: "400" }}>Hatim paylaşılan kişiler SMS ile bilgilendirilecektir. Lütfen bilgileri doğru girdiğinizden emin olunuz.</Text>
               </View>
             </View>
-          </ScrollView>
-        </View>
-
+          
+            </ScrollView>
       </View>
+      
 
       <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
-        <TouchableOpacity >
-          <View style={{ backgroundColor: "#163E6C", width: SCREEN_WIDTH - 30, height: 60, marginVertical: 15, borderRadius: 10, justifyContent: "center", alignItems: "center", alignSelf: "center", }}>
+        <TouchableOpacity>
+          <View style={{ backgroundColor: "#163E6C", width: SCREEN_WIDTH - 30, height: 60, marginVertical: 15, borderRadius: 10, justifyContent: "center", alignItems: "center", alignSelf: "center" }}>
             <Text style={{ color: "white", fontFamily: "OpenSans-Regular", fontSize: 16, fontWeight: "600" }}>Hatim Oluştur / Paylaş</Text>
           </View>
         </TouchableOpacity>
       </View>
 
       <HelpDeskSheet isVisible={modalVisible} toggleModal={toggleModal} />
-      <HatimDavetModal isVisibleM={modalVisibleM} toggleDonateItemsList={toggleModalM}></HatimDavetModal>
+      <HatimDavetModal isVisibleM={modalVisibleM} toggleDonateItemsList={toggleModalM} />
     </>
-  )
+  );
 }
